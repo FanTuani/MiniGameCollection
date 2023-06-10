521f07f3b3166f0d8d95b4cf5f6b7bb875d2b254
RiceQuakes <2486921393@qq.com>
2023-06-10 22:48:39 +0800

    Init

---
+++ src/main/java/lwq/minigamecollection/MiniGameCollection.java
@@ -0,0 +1,17 @@
+package lwq.minigamecollection;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MiniGameCollection extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
